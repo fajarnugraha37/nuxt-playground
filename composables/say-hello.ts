313064ef8feb2a0b8650a4107f7d59491ffa104a
fajar <nugrahafajar37@gmail.com>
2025-05-04 22:56:11 +0700

    init project

---
+++ composables/say-hello.ts
@@ -0,0 +1,3 @@
+export function sayHello(message = 'World') {
+    alert(`Hello ${message.toUpperCase()}!`)
+}
---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+
+    modules: [
+        '@nuxt/fonts',
+        '@nuxt/icon',
+        '@nuxt/image',
+        '@nuxt/content',
+        '@nuxt/ui',
+        '@nuxt/scripts',
+        '@pinia/nuxt',
+        'pinia-plugin-persistedstate/nuxt',
+    ],
+    css: ['~/assets/css/main.css'],
+    pinia: {
+        storesDirs: [
+            './stores/**',
+        ],
+    }
+})
